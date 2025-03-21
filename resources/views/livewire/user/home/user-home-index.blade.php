11f24269539915ded8b7ed82f002968dbd65bbc0
brehehe <bratang1h@gmail.com>
2025-03-21 12:43:30 +0700

    Update

--- resources/views/livewire/user/home/user-home-index.blade.php
+++ resources/views/livewire/user/home/user-home-index.blade.php
@@ -13,7 +13,7 @@
                 <div class="grid grid-cols-6 gap-4 mt-6">
                     @foreach ($cakes as $cake)
                         <div wire:click="choiceCake({{ $cake->id }})"
-                            class="bg-main flex items-center justify-center  rounded-full p-3 {{ $cake->id == $get_cake->id ? 'border-4 border-border-search' : null }} hover:border-4 hover:border-border-search transition-all ease-out duration-400">
+                            class="bg-main flex items-center justify-center  rounded-full p-3 {{ $cake->id == $get_cake?->id ? 'border-4 border-border-search' : null }} hover:border-4 hover:border-border-search transition-all ease-out duration-400">
                             <img src="{{ asset('storage/' . $cake->image) }}" alt="">
                         </div>
                     @endforeach
@@ -25,16 +25,17 @@
             <div class=" w-1/2 flex flex-col p-4 gap-4">
 
                 <div class="bg-gradient-to-b from-white to-nav-main px-4 py-10 rounded-2xl">
-                    <h1 class="text-4xl text-main-menu">{{ $get_cake->name }}</h1>
-                    <h1 class="mt-4 text-2xl text-main-menu">Rp. {{ number_format($get_cake->price, 0, ',', '.') }}</h1>
+                    <h1 class="text-4xl text-main-menu">{{ $get_cake?->name }}</h1>
+                    <h1 class="mt-4 text-2xl text-main-menu">Rp. {{ number_format($get_cake?->price, 0, ',', '.') }}
+                    </h1>
                     <p class="mt-4 font-plus-jakarta-sans text-main-menu">
-                        {{ $get_cake->description }}
+                        {{ $get_cake?->description }}
                     </p>
                     <div class="mt-4 font-plus-jakarta-sans text-main-menu">
                         <h4>Ingredients : </h4>
-                        @if ($get_cake->ingredients && $get_cake->ingredients->isNotEmpty())
+                        @if ($get_cake?->ingredients && $get_cake?->ingredients->isNotEmpty())
                             <ul class="list-disc ml-6">
-                                @foreach ($get_cake->ingredients as $ingredient)
+                                @foreach ($get_cake?->ingredients as $ingredient)
                                     <li>{{ $ingredient->name }}</li>
                                 @endforeach
                             </ul>
